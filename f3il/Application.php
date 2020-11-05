1c3d1c85686d3eb5ca771c6f29aa37e3478d916d
Armel JONDO <armel.jondo@lmtgroup.com>
2020-11-05 14:17:17 +0100

    correction de l'initialisation du logger et ajout du dumper

--- f3il/Application.php
+++ f3il/Application.php
@@ -1,83 +1,93 @@
 <?php
+
 namespace f3il;
 
-use \Monolog\Logger;
-use \Monolog\Handler\StreamHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 
-class Application{
+class Application {
+    public static $DEBUG_MODE = 'debug';
+    public static $PRODUCTION_MODE = 'production';
 
-
-    public const DEBUG_MODE = 'debug';
-    public const PRODUCTION_MODE = 'production';
-
-    private static $_instance =null;
+    private static $_instance = null;
 
     private $defaultControllerName = null;
-    private $runMode = self::PRODUCTION_MODE;
+    private $runMode = 'production';
     private $logger = null;
 
-    private function __contruct(){
+    private function __contruct()
+    {
         $this->startLogger();
         $this->setRunMode();
     }
 
-    public function startLogger(){
+    public function startLogger()
+    {
         $this->logger = new Logger('f3il');
-        $this->logger->pushHandler(new StreamHandler('log/app.log',Logger::DEBUG);
-        $this->logger->addInfo('App started');
+        $this->logger->pushHandler(new StreamHandler(APP_FOLDER . '/../log/app.log', Logger::DEBUG));
+        $this->logger->info('App started');
     }
 
-    public function getLogger(){
+    public function getLogger()
+    {
         return $this->logger;
     }
-        public function setRunMode(){
-            $conf= Configuration::getInstance();
-            if(isset($conf->run_mode)&& $conf->run_mode===self::DEBUG_MODE){
-                $this->runMode=self::DEBUG_MODE;
-                \error_reporting(E_ALL);
-            }else{
-                \error_reporting(0);
-            }
-        }
 
-        public function getRunMode(){
-            return $this->runMode;
+    public function setRunMode()
+    {
+        $conf = Configuration::getInstance();
+        if (isset($conf->run_mode) && $conf->run_mode === self::DEBUG_MODE) {
+            $this->runMode = self::DEBUG_MODE;
+            \error_reporting(E_ALL);
+        } else {
+            \error_reporting(E_ALL);
         }
-        public static function getInstance(){
-            if(is_null(self::$_instance)){
-                self::$_instance = new Application();
-            }
-            return self::$_instance;
-        }
+    }
 
-        public static function getControllerClass($controllerName)
-        {
-            if($controllerName == 'error'){
-                return 'f3il\\ErrorController';
-            }
-            $controllerClass=
-            APP_NAMESPACE.'\Controllers\\'.$controllerName.'Controller';
-            if(!class_exists($controllerClass)){
-                Error('Application : controleur introuvable'.$controllerClass);
-            }
-            return $controllerClass;
-        }
+    public function getRunMode()
+    {
+        return $this->runMode;
+    }
 
-        public function setDefaultControllerName($controllerName)
-        {
-            $this->defaultControllerName = $controllerName;
+    public static function getInstance()
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new Application();
+            self::$_instance->startLogger();
         }
+        return self::$_instance;
+    }
 
-        public function run()
-        {
-         try{
+    public static function getControllerClass($controllerName)
+    {
+        if ($controllerName == 'error') {
+            return 'f3il\\ErrorController';
+        }
+        $controllerClass = APP_NAMESPACE . '\controllers\\' . $controllerName . 'Controller';
+        if (!class_exists($controllerClass)) {
+            self::Error('Application : controleur introuvable' . $controllerClass);
+        }
+        return $controllerClass;
+    }
+
+    public static function Error($message) {
+        self::$_instance->logger->error($message);
+    }
+
+    public function setDefaultControllerName($controllerName)
+    {
+        $this->defaultControllerName = $controllerName;
+    }
+
+    public function run()
+    {
+        try {
             //recuperation dans GET du parametre controller
-            $controllerName = filter_input(INPUT_GET,'controller');
-            if(is_null($controllerName)){
-                if(is_null($this->defaultControllerName))
-                {
+            $controllerName = filter_input(INPUT_GET, 'controller');
+            if (is_null($controllerName)) {
+                if (is_null($this->defaultControllerName)) {
                     throw new Error('Application : aucun controleur renseigné');
-                } else{
+                } else {
                     $controllerName = $this->defaultControllerName;
                 }
             }
@@ -86,11 +96,11 @@ class Application{
             $controllerClass = $this->getControllerClass($controllerName);
 
             //contruction de l'objet
-            $controller= new $controllerClass();
+            $controller = new $controllerClass();
 
             //recuperation de l'action à excécuter
-            $actionName = filter_input(INPUT_GET,'action');
-            if(is_null($actionName)){
+            $actionName = filter_input(INPUT_GET, 'action');
+            if (is_null($actionName)) {
                 $actionName = $controller->getDefaultActionName();
                 //die('Application : aucune action renseignée');
 
@@ -101,31 +111,33 @@ class Application{
 
             //rendu de la page
             Page::getInstance()->render();
-        }catch(Error $exp){
+        } catch (Error $exp) {
             $exp->render();
-        }catch(\Exception $exp){
+        } catch (\Exception $exp) {
             $this->logger->addError($exp->getMessage());
-            if($this -> runMode == self::DEBUG_MODE){
+            if ($this->runMode == self::DEBUG_MODE) {
                 throw $exp;
-            }else{
+            } else {
                 self::redirect('?controller=error');
             }
         }
-           // $controllerName ="materiels";
-            //echo $this->getControllerClass($controllerName);
-        }
-        //function de redirection
-        public function redirect($url){
-            if(!headers_sent()){
-                header ("HTTP/1.1 303 see Other");
-                header ("Location: ".$url);
-                die();
-            }else {
+        // $controllerName ="materiels";
+        //echo $this->getControllerClass($controllerName);
+    }
+
+    //function de redirection
+    public function redirect($url)
+    {
+        if (!headers_sent()) {
+            header("HTTP/1.1 303 see Other");
+            header("Location: " . $url);
+            die();
+        } else {
             ?>
             <script>
-                Window.Location='<?php echo $url; ?>';
-            </script>   
-     <?php
+                Window.Location = '<?php echo $url; ?>';
+            </script>
+            <?php
+        }
     }
-}   
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,8 +4,14 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInitf77add522b0d62dfaf00c8d5bdeacaaf
+class ComposerStaticInitfbc99a021868872a3807dfcb1e1385d7
 {
+    public static $files = array (
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
         'f' => 
         array (
@@ -14,6 +20,12 @@ class ComposerStaticInitf77add522b0d62dfaf00c8d5bdeacaaf
         'a' => 
         array (
             'app\\' => 4,
+        ),
+        'S' => 
+        array (
+            'Symfony\\Polyfill\\Php80\\' => 23,
+            'Symfony\\Polyfill\\Mbstring\\' => 26,
+            'Symfony\\Component\\VarDumper\\' => 28,
         ),
         'P' => 
         array (
@@ -34,6 +46,18 @@ class ComposerStaticInitf77add522b0d62dfaf00c8d5bdeacaaf
         array (
             0 => __DIR__ . '/../..' . '/application',
         ),
+        'Symfony\\Polyfill\\Php80\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
+        ),
+        'Symfony\\Polyfill\\Mbstring\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
+        ),
+        'Symfony\\Component\\VarDumper\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/var-dumper',
+        ),
         'Psr\\Log\\' => 
         array (
             0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
@@ -44,11 +68,20 @@ class ComposerStaticInitf77add522b0d62dfaf00c8d5bdeacaaf
         ),
     );
 
+    public static $classMap = array (
+        'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+        'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
+        'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInitf77add522b0d62dfaf00c8d5bdeacaaf::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInitf77add522b0d62dfaf00c8d5bdeacaaf::$prefixDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitfbc99a021868872a3807dfcb1e1385d7::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitfbc99a021868872a3807dfcb1e1385d7::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInitfbc99a021868872a3807dfcb1e1385d7::$classMap;
 
         }, null, ClassLoader::class);
     }
